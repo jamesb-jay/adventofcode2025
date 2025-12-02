616e88b1e0628d00d024d194d0e916e2e81d0103
James Bennett <21203355+jamesb-jay@users.noreply.github.com>
2025-12-02 03:16:29 +0000

    day 1, part 1

---
+++ advent.py
@@ -0,0 +1,56 @@
+
+
+
+''' Day 1 '''
+
+class Dial:
+    position = 50
+
+    def get_effective_turn(amount: int) -> int:
+        # Correct for 0 > amount > 100
+        delta = abs(amount)
+        effective = delta - (delta % 100)
+        amount += effective if amount < 0 else -effective
+        return amount
+    
+    def correct_position(p):
+        if p < 0: return p + 100
+        elif p > 99: return p - 100
+        return p
+
+    def turn(self, amount: int):
+        newpos = self.position
+
+        newpos += Dial.get_effective_turn(amount)
+        newpos = Dial.get_effective_turn(newpos)
+        newpos = Dial.correct_position(newpos)
+
+        # print(f"At {self.position}, turning {amount} ({Dial.get_effective_turn(amount)}) to {newpos}")
+
+        self.position = newpos
+
+def day1():
+    with open("day1.txt") as f:
+        rotations = parsePuzzleinput(f.readlines())
+    
+    dial = Dial()
+
+    zeros = 0
+    for r in rotations:
+        dial.turn(r)
+        if dial.position == 0:
+            zeros += 1
+
+    return zeros
+    
+
+
+''' Go from e.g "R50 L40" to "50, -40'''
+def parsePuzzleinput(lines: list[str]) -> list[int]:
+    for l in lines:
+        value: int = int(l[1:])
+        yield value if l.startswith("R") else -value
+
+
+if __name__ == "__main__":
+    print(f"Day 1 result: {day1()}")
