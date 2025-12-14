d99dbec7ef008f52c0974ca9f1c156d7ce27f894
James Bennett <21203355+jamesb-jay@users.noreply.github.com>
2025-12-14 17:34:26 +0000

    updates to d1

--- advent.py
+++ advent.py
@@ -1,76 +1,43 @@
-
-
-
-''' Day 1 '''
+''' Day 1 : this is terrible'''
 
 class Dial:
     position = 50
 
-    def get_effective_turn(amount: int) -> int:
-        # Correct for 0 > amount > 100
-        delta = abs(amount)
-        effective = delta - (delta % 100)
-        amount += effective if amount < 0 else -effective
-        return amount
-    
-    def correct_position(p):
-        if p < 0: return p + 100
-        elif p > 99: return p - 100
-        return p
-
     def turn(self, amount: int):
-        newpos = self.position
+        self.position += amount
+        self.position = self.position - (100*(self.position//100))
 
-        newpos += Dial.get_effective_turn(amount)
-        newpos = Dial.get_effective_turn(newpos)
-        newpos = Dial.correct_position(newpos)
+        if self.position < 0: return self.position + 100
+        elif self.position > 99: return self.position - 100
 
-        # print(f"At {self.position}, turning {amount} ({Dial.get_effective_turn(amount)}) to {newpos}")
 
-        self.position = newpos
+def parseday1(lines: list[str]):
+    for l in lines:
+        value: int = int(l[1:])
+        yield value if l.startswith("R") else -value
 
-def day1():
-    with open("day1.txt") as f:
-        rotations = parsePuzzleinput(f.readlines())
-    
+def day1(rotations):
     dial = Dial()
-
     zeros = 0
     for r in rotations:
         dial.turn(r)
         if dial.position == 0:
             zeros += 1
-
     return zeros
 
-def day1part2():
-    with open("day1.txt") as f:
-        rotations = parsePuzzleinput(f.readlines())
-    
+def day1part2(rotations):
     dial = Dial()
-
     zeros = 0
     for r in rotations:
-
         for _ in range(abs(r)):
-            c = 1 if r > 0 else -1
-
-            dial.turn(c)
-
+            dial.turn(1 if r > 0 else -1)
             if dial.position == 0:
                 zeros += 1
-
     return zeros
-    
-
-
-''' Go from e.g "R50 L40" to "50, -40'''
-def parsePuzzleinput(lines: list[str]) -> list[int]:
-    for l in lines:
-        value: int = int(l[1:])
-        yield value if l.startswith("R") else -value
-
 
 if __name__ == "__main__":
-    print(f"Day 1 part 1 result: {day1()}")
-    print(f"Day 1 part 2 result: {day1part2()}")
+    with open("day1.txt") as f:
+        rotations = parseday1(f.readlines())
+    
+    print(f"Day 1 part 1 result: {day1(rotations)}")
+    print(f"Day 1 part 2 result: {day1part2(rotations)}")
